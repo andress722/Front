de0b32d51401c50727986d1324e8b694156c3fa1
Andre Moreira Cidre <andrecidre@hotmail.com>
2023-11-27 21:43:53 -0300

    vercel modifications to build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import Script from 'next/script';
+
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -20,7 +22,10 @@ useEffect(() => {
     
   
       <Header selectedCategory={selectedCategory} onCategorySelect={setSelectedCategory} />
-      
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous" />
+      <Script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></Script>
+      <Script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></Script>
           <div style={{ minHeight: 'calc(100vh - 104px)', display: 'flex', flexDirection: 'column' }}>
             <Component {...pageProps} selectedCategory={selectedCategory} />
           </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,10 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-    
-      <Script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></Script>
-      <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></Script>
-      <Script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></Script>
+      
       
       </Head>
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossOrigin="anonymous" />
